2bf5a6de193f36bda423bb5ba3886baef4d69f3f
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-01-01 19:50:30 +0530

    image hide code written to content.js file

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,12 @@
-// Receive message from background script
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log(request.message);
-  console.log(" Time ", new Date().toString());
+// Listen for the click event from background script
+chrome.runtime.onMessage.addListener(() => {
+  // Hide all the available images of the page
+  const images = document.getElementsByTagName("img");
+  for (const image of images) image.style.display = "none";
+  // Hide all the background images of the page
+  const bgImages = document.getElementsByTagName("*");
+  for (const bgImage of bgImages) bgImage.style.backgroundImage = "none";
+  // Hide all the svg images of the page
+  const svgImages = document.getElementsByTagName("svg");
+  for (const svgImage of svgImages) svgImage.style.display = "none";
 });
