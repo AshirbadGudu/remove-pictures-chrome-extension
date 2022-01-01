94cbb8fcc3e3ff8f9ae228e4cbcb2256c8cb42f8
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-01-01 17:35:40 +0530

    onclick event added to the browser action

--- scripts/background.js
+++ scripts/background.js
@@ -1,1 +1,7 @@
 console.log("Running background.js");
+
+// Add onclick event to the browser action
+chrome.browserAction.onClicked.addListener((tab) => {
+  // Send a message to the active tab
+  console.log(tab);
+});
