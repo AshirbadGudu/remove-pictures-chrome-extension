0b2bfe9feb9efb952e419c076f57df02f0d57bc6
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-01-01 19:13:46 +0530

    Background Script and content Script Connection complete

--- scripts/background.js
+++ scripts/background.js
@@ -3,5 +3,7 @@ console.log("Running background.js");
 // Add onclick event to the browser action
 chrome.browserAction.onClicked.addListener((tab) => {
   // Send a message to the active tab
-  console.log(tab);
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { message: "clicked_browser_action" });
+  });
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,5 @@
-console.log("Hello world From Chrome Extension at ", new Date().toString());
+// Receive message from background script
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log(request.message);
+  console.log(" Time ", new Date().toString());
+});
